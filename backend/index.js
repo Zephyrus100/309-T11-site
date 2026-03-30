f7c56af0b08f3312cd5ae4025d9f949fb3d96e8b
mattm <mattmike2004@gmail.com>
2026-03-30 19:51:28 -0400

    :)

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import routes from "./routes.js";
+import dotenv from "dotenv";
+import cors from "cors";
+// TODO: complete me (loading the necessary packages)
+dotenv.config();
+
+const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+// TODO: complete me (CORS)
+app.use(cors())
+app.use(express.json());
+app.use('', routes);
+
+export default app;
---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,124 @@
+import React, { createContext, useContext, useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AuthContext = createContext(null);
+
+// TODO: get the BACKEND_URL.
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+
+/*
+ * This provider should export a `user` context state that is 
+ * set (to non-null) when:
+ *     1. a hard reload happens while a user is logged in.
+ *     2. the user just logged in.
+ * `user` should be set to null when:
+ *     1. a hard reload happens when no users are logged in.
+ *     2. the user just logged out.
+ */
+export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate();
+    // const user = null; // TODO: Modify me.
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        // TODO: complete me, by retriving token from localStorage and make an api call to GET /user/me.
+        const token = localStorage.getItem("token");
+        if (!token) {
+            setUser(null);
+            return;
+        }
+
+        fetch(`${VITE_BACKEND_URL}/user/me`, {method: "GET", headers: {"Authorization": `Bearer ${token}`}})
+        .then(res => res.json())
+        .then(json => {
+            if (!json) {
+                localStorage.removeItem("token");
+                setUser(null);
+                return;
+            }
+            setUser(json.user)
+        });
+    }, [])
+
+    /*
+     * Logout the currently authenticated user.
+     *
+     * @remarks This function will always navigate to "/".
+     */
+    const logout = () => {
+        localStorage.removeItem("token");
+        setUser(null);
+        navigate("/");
+        return;
+    };
+
+    /**
+     * Login a user with their credentials.
+     *
+     * @remarks Upon success, navigates to "/profile". 
+     * @param {string} username - The username of the user.
+     * @param {string} password - The password of the user.
+     * @returns {string} - Upon failure, Returns an error message.
+     */
+    const login = async (username, password) => {
+        // TODO: complete me
+        const res = await fetch(`${VITE_BACKEND_URL}/login`, {
+            method: "POST", 
+            headers: {"Content-Type": "application/json"}, 
+            body: JSON.stringify({ username, password })
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data.message;
+        }
+
+        const userRes = await fetch(`${VITE_BACKEND_URL}/user/me`, {
+            headers: {"Authorization": `Bearer ${data.token}`}
+        });
+
+        const userData = await userRes.json();
+        if (!userRes.ok) {
+            return userData.message;
+        }
+
+        localStorage.setItem("token", data.token);
+        setUser(userData.user);
+        navigate("/profile");
+        return;
+    };
+
+    /**
+     * Registers a new user. 
+     * 
+     * @remarks Upon success, navigates to "/".
+     * @param {Object} userData - The data of the user to register.
+     * @returns {string} - Upon failure, returns an error message.
+     */
+    const register = async (userData) => {
+        // TODO: complete me
+        const res = await fetch(`${VITE_BACKEND_URL}/register`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(userData)
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+            return data.message;
+        }
+
+        navigate("/")
+        return;
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout, register }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
